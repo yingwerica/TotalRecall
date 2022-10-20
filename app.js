b70467f4f13630fa916df4ea1060f836ba6dfbf8
yingwerica <yingw.pro@gmail.com>
2022-10-20 16:21:55 -0600

    second commit

--- app.js
+++ app.js
@@ -54,22 +54,143 @@ isCow = animal == "cow" ? 'mooooo' : "Hey! You're not a cow.";
 console.log(isCow);
 
 //Driver's Ed
-
-
+let age = 15;
+age >= 16 ? console.log("Here are the keys!") : console.log("Sorry, you're too young.");
 
 //LOOPS
-
 //The Basics
+for (let i = 0; i <= 10; i++) {
+    console.log(i); 
+};
+for (let i = 10; i <= 400; i++) {
+    console.log(i); 
+};
+for (let i = 12; i <= 4000; i+=3) {
+    console.log(i); 
+};
 
 //Get Even
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(`${i} <-- is an even number`);
+    } else {
+        console.log(i);
+    }
+};
 
 //Give Me 5
+for (let i = 0; i <= 100; i++) {
+     if (i % 5 == 0 && i % 3 !== 0 && i !== 0 ) {
+         console.log(`I found a ${i}. High five!`);
+     } else if (i % 3 == 0 && i % 5 !== 0 && i !== 0) {
+         console.log(`I found a ${i}. Three is a crowd`);
+     } else if (i % 3 == 0 && i % 5 == 0 && i !== 0) {
+         console.log(`I found a ${i}. Three is a crowd.I found a ${i}. High five! `);
+     }
+};
 
 //Savings Account
-
+let bank_account = 0;
+for (let i = 1; i <= 100; i++) {
+   bank_account += i*2;
+}
+console.log(bank_account);
 
 
 //ARRAYS & CONTROL FLOW
+/*
+What are the things in an array called?
+It is called element, can be different datatypes.
+Do Arrays guarantee those things will be in order?
+Yes. Array are ordered.
+What real-life thing could you model with an array?
+A list of items. E.g., shopping list.
+*/
 
+//Easy Does It
+const quotes = ['','',''];
+
+// Accessing elements
+const randomThings = [1, 10, "Hello", true];
+console.log(randomThings[0]);
+randomThings[2] = "World";
+console.log(randomThings);
+
+//Change values
+const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"];
+ourClass[2];
+ourClass[4] = "Octocat";
+ourClass.push('Cloud City') ;
+console.log(ourClass);
+
+//Mix It Up
+const myArray = [5, 10, 500, 20];
+myArray.push("Aegon");
+myArray.push("Great");
+myArray.shift();
+myArray.unshift("Bob Marley");
+myArray.pop();
+console.log(myArray);
+console.log(myArray.reverse());
+console.log(myArray);
+
+//Did you mutate the array with array.reverse( ) ? What does mutate mean? Did the .reverse()method return anything?
+//Yes. The reverse() method transposes the elements of the calling array object in place, mutating the array, and returning a reference to the array. A value is said to be mutable if it can be changed.  A mutation is the act of changing the properties of a value. 
+
+//Biggie Smalls
+let int = 9;
+//int < 100 ? console.log('little number') : console.log('big number');
+if ( int < 100) {
+    console.log('little number');
+} else {
+    console.log('big number');
+}
+
+//Monkey in the Middle
+/*int < 5 ? console.log('little number') 
+    : int > 10? console.log('big number')
+    : console.log('monkey');
+    */
+
+if ( int < 5) {
+     console.log('little number');
+ } else if( int > 10) {
+     console.log('big number');  
+ } else {
+     console.log('monkey');
+ }
+
+
+//What's in Your Closet?
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "GA hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
 
 
